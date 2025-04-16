70235acc1b95c05730ade40442a69b39b7978b2d
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-04-17 00:07:52 +0200

    2

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Subject,Course,Module
 # Register your models here.
+@admin.register(Subject)
+class SubjecAdmin(admin.ModelAdmin):
+    all_fields = [f.name for f in Subject._meta.fields]  #+["id"]                   #SHOW ALL FILDES
+    list_display=all_fields
+    prepopulated_fields = {'slug': ('title',)}
+
+class ModuleInLine(admin.StackedInline):
+    model=Module
+    # all_fields = [f.name for f in Module._meta.fields]  #+["id"]                   #SHOW ALL FILDES
+    # list_display=all_fields
+    
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+   
+    all_fields = [f.name for f in Module._meta.fields]  #+["id"]                   #SHOW ALL FILDES
+    list_display=all_fields
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+     all_fields = [f.name for f in Course._meta.fields]  #+["id"]                   #SHOW ALL FILDES
+     list_display=all_fields
+     list_filter=["created"]
+     search_fields=['title','overview']
+     prepopulated_fields = {'slug': ('title',)}
+     inlines=[ModuleInLine]
+# admin.site.register(Course)
--- course/models.py
+++ course/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Subject(models.Model): #categorías
+    title=models.CharField(max_length=250)
+    description=models.TextField(blank=True)
+    slug=models.SlugField(max_length=250,unique=True)
+    class Meta:
+        ordering=["title"]
+    def __str__(self):
+        return self.title
+    
+class Course(models.Model): 
+    class Status(models.TextChoices):
+        AVAILABLE='AV','Avsilable'
+        DEREFT='DF','Dreft'
+    owner=models.ForeignKey(User,related_name="courses_created",on_delete=models.CASCADE)
+    subject=models.ForeignKey(Subject,related_name="courses",on_delete=models.CASCADE)
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250,unique=True)
+    overview=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=2 ,choices=Status,default=Status.AVAILABLE)
+    class Meta:
+        ordering=["-created"]
+    def __str__(self):
+        return self.title        
+
+
+
+class Module(models.Model):
+    course=models.ForeignKey(Course,related_name="modules",on_delete=models.CASCADE)
+    title=models.CharField(max_length=250)
+    description=models.TextField(blank=True)
+    def __str__(self):
+        return self.title    
