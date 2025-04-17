7f78d32209e8e598df1b41e0fbd6bea311e2bbf3
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-04-17 18:06:15 +0200

    4

--- course/models.py
+++ course/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 # Create your models here.
 class Subject(models.Model): #categorías
     title=models.CharField(max_length=250)
@@ -34,3 +38,31 @@ class Module(models.Model):
     description=models.TextField(blank=True)
     def __str__(self):
         return self.title    
+    
+class Content(models.Model):
+    module=models.ForeignKey(Module,related_name="contents",on_delete=models.CASCADE)
+    content_type=models.ForeignKey(ContentType,on_delete=models.CASCADE,limit_choices_to={'model__in':('text','file','image','video')})
+    objects_id=models.PositiveIntegerField()
+    item=GenericForeignKey("content_type","objects_id")    
+
+
+class ItemBase(models.Model):
+    owner=models.ForeignKey(User,related_name="%(class)s_related",on_delete=models.CASCADE) 
+    title=models.CharField(max_length=250) 
+    creates=models.DateTimeField(auto_now_add=True)  
+    update=models.DateTimeField(auto_now=True) 
+    class Meta:
+        abstract=True
+
+    def __str__(self):
+        return self.title     
+    
+class Text(ItemBase):
+    text=models.TextField()
+class File(ItemBase):
+    file=models.FileField(upload_to="file")
+class Image(ItemBase):
+    image=models.ImageField(upload_to="images")
+class Video(ItemBase):
+    video=models.URLField()
+    
