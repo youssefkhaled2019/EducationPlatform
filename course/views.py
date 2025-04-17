02f0d4d6564a3f4b0481610a993e604fea25b036
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-04-17 19:42:14 +0200

    5

--- course/views.py
+++ course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Subject,Course,Content
 # Create your views here.
+
+# def index(request):
+#    return render(request,"x.html")
+def subject_courses_list(request):
+   subjects=Subject.objects.prefetch_related("courses").all()
+#    print(subjects)
+   return render(request,"course/subject_course_list.html",{"subjects":subjects})
